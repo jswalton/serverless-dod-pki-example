5b16678c2e11763559e8586ed852f7e9662a9005
Joshua Walton <joshuawalton@Joshuas-MacBook-Pro.local>
2021-09-30 16:39:56 -0700

    initial commit

---
+++ lib/serverless-dod-pki-example-stack.ts
@@ -0,0 +1,78 @@
+import * as cdk from '@aws-cdk/core';
+import { ConnectionType, Integration, IntegrationType, RestApi, SecurityPolicy, VpcLink } from '@aws-cdk/aws-apigateway';
+import { Bucket } from '@aws-cdk/aws-s3';
+import { Certificate } from '@aws-cdk/aws-certificatemanager';
+import { ContainerImage } from '@aws-cdk/aws-ecs';
+import { NetworkLoadBalancedFargateService } from '@aws-cdk/aws-ecs-patterns';
+import { Port, SubnetType, Vpc } from '@aws-cdk/aws-ec2';
+import { BucketDeployment, Source } from '@aws-cdk/aws-s3-deployment';
+import { CfnParameter } from '@aws-cdk/core';
+
+export class ServerlessCacProxyTestStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const domainName = new CfnParameter(this, "domainName", {
+      type: "String",
+      description: "The name of the domain that will be CaC enabled"}
+    );
+
+    const domainCertificateARN = new CfnParameter(this, "domainCertificateARN", {
+      type: "String",
+      description: "The ARN of the domain SSL certificate that has been imported in AWS certificate Manager"}
+    );
+
+    const certificateBucket = new Bucket(this, 'mtls-certificate-bucket');
+    
+    new BucketDeployment(this, 'DeployFiles', {
+      sources: [Source.asset('./lib/dod-truststore')],
+      destinationBucket: certificateBucket,
+      retainOnDelete: false
+    });
+
+    const vpc = new Vpc(this, 'CaCEnabledServicesVPC', {});
+    
+    const loadBalancedFargateService = new NetworkLoadBalancedFargateService(this, 'example-service', {
+      vpc,
+      memoryLimitMiB: 1024,
+      cpu: 512,
+      taskImageOptions: {
+        image: ContainerImage.fromRegistry("amazon/amazon-ecs-sample"),
+      },
+      taskSubnets: {
+        subnetType: SubnetType.PRIVATE_WITH_NAT
+      },
+      publicLoadBalancer: false,
+    });
+
+    loadBalancedFargateService.service.connections.allowFromAnyIpv4(Port.allTraffic())
+
+    const cacEnabledRestAPI = new RestApi(this, 'some-api', {
+      domainName: {
+        certificate: Certificate.fromCertificateArn(this, "domain-cert", domainCertificateARN.valueAsString),
+        domainName: domainName.valueAsString,
+        securityPolicy: SecurityPolicy.TLS_1_2,
+        mtls: {
+          bucket: certificateBucket,
+          key: "truststore.pem",
+        }
+      },
+    });
+
+    const link = new VpcLink(this, 'link', {
+      targets: [loadBalancedFargateService.loadBalancer],
+    });
+    
+    const ecsFargateBackend = new Integration({
+      type: IntegrationType.HTTP_PROXY,
+      integrationHttpMethod: 'ANY',
+      options: {
+        connectionType: ConnectionType.VPC_LINK,
+        vpcLink: link,
+      },
+    });
+
+    cacEnabledRestAPI.root.addMethod('ANY', ecsFargateBackend)
+
+  }
+}
